a925c558e4db46b54050d84ddd8556dc6f4e3ff9
Hridyanshu Gupta <ghridyanshu@gmail.con>
2026-03-30 16:27:15 +0530

    fix style

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,13 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from "./context/ThemeContext";
 
-
 createRoot(document.getElementById('root')).render(
-  <ThemeProvider>
-  <App />
-  </ThemeProvider>,
+  <React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 )
